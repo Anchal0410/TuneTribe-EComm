bc46ea2841d241a276b8825fcc6d3c25d5f56d03
Anchal0410 <jainanchal0410@gmail.com>
2024-12-14 00:44:32 +0530

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -7,7 +7,15 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
+    // Add CORS middleware configuration
+    strapi.config.middleware.settings.cors = {
+      enabled: true,
+      origin: ['https://tune-tribe-e-com.vercel.app/'], // Allowed origins
+      headers: '*', // Allowed headers
+      credentials: true
+    };
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -16,5 +24,7 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+    console.log('Strapi application is starting...');
+  },
 };
